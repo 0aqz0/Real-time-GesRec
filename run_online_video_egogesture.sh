14c4a95e04802733188ad06b6973e44bcd77a6e5
0aqz0 <aqz@zju.edu.cn>
2021-03-17 21:39:58 +0800

    update param

--- run_online_video_egogesture.sh
+++ run_online_video_egogesture.sh
@@ -6,10 +6,10 @@ python online_test_video.py \
 	--root_path ~/projects/reference/Real-time-GesRec \
 	--resume_path_det results/egogesture_resnetl_10_RGB_8.pth \
 	--resume_path_clf results/egogesture_resnext_101_RGB_32.pth  \
-    --video $1 \
+    --video 0 \
 	--sample_duration 8 \
 	--sample_duration_det 8 \
-	--sample_duration_clf 8 \
+	--sample_duration_clf 32 \
 	--model_det resnetl \
 	--model_clf resnext \
 	--model_depth_det 10 \
@@ -32,6 +32,6 @@ python online_test_video.py \
 	--det_counter 2 \
 	--clf_strategy median \
 	--clf_queue_size 32 \
-	--clf_threshold_pre 0 \
+	--clf_threshold_pre 1.0 \
 	--clf_threshold_final 0.15 \
 	--stride_len 1
